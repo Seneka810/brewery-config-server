a418fce83923f1932ea4de0dcbaf7383b9c99982
Stas Kordyk <stas.kordyk@gmail.com>
2022-04-29 17:01:45 +0300

    project init

---
+++ src/test/java/com/kord/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kord.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
